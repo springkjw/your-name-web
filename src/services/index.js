4c663412b0199fca379d5a2b91bd47918aecf5b4
Kwon Jaewon <springkjw@gmail.com>
2021-07-10 03:17:47 +0900

    isLoggedIn와  localStorage 연결

---
+++ src/services/index.js
@@ -0,0 +1,4 @@
+export { default as api } from './api';
+export * from './auth';
+export * from './member';
+export * from './message';
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 export const userSlice = createSlice({
   name: 'user',
   initialState: {
-    isLoggedIn: false,
+    isLoggedIn: !!localStorage.getItem('accessToken'),
   },
   reducers: {
     doLogin: () => {},
