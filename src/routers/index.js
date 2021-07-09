b6f0130e3a6911e8332a38f78b2abf0732c8c589
Kwon Jaewon <springkjw@gmail.com>
2021-07-10 03:33:17 +0900

    firebase 패키지 설치

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import firebase from 'firebase';
 
 import { withStore } from '@store';
 import { theme, HomePage } from '@components';
 import AuthRouter from './auth';
+
+const config = {
+  apiKey: '',
+  authDomain: 'imap-push-server.firebaseapp.com',
+  databaseURL: 'https://imap-push-server.firebaseio.com',
+  projectId: 'imap-push-server',
+  storageBucket: 'imap-push-server.appspot.com',
+  messagingSenderId: '',
+};
+
+firebase.initializeApp(config);
 
 function App() {
   return (
