c757c0fa2aeb300639587aa9df1edd0582d5398b
Kwon Jaewon <springkjw@gmail.com>
2021-07-10 03:24:21 +0900

    BrowserRouter 적용

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
 import { withStore } from '@store';
-import { theme } from '@components';
+import { theme, HomePage } from '@components';
+import AuthRouter from './auth';
 
 function App() {
-  return <ThemeProvider theme={theme} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/">
+            <HomePage />
+          </Route>
+          <Route path="/auth">
+            <AuthRouter />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default withStore(App);
